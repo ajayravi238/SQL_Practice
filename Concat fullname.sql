066c2206248a33a6202a377b9f2109ce1a0b6b10
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-08-24 09:30:53 +0530

    Update Concat fullname.sql

--- Concat fullname.sql
+++ Concat fullname.sql
@@ -1,4 +1,4 @@
 -- Show first name and last name concatinated into one column to show their full name.
 
-select concat first_name, ' ', last_name as full_name
+select concat (first_name, ' ', last_name) as full_name
 from patients;
