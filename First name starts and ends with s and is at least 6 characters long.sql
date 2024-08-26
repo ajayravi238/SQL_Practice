04ec0a9c4756aade5286d0ceb3c1159c0bec678a
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-08-26 10:16:37 +0530

    Create First name starts and ends with s and is at least 6 characters long

---
+++ First name starts and ends with s and is at least 6 characters long.sql
@@ -0,0 +1,5 @@
+-- Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
+
+SELECT patient_id, first_name
+FROM patients
+WHERE first_name LIKE 's____%s';
