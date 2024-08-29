b6fbb502a26d5d57b85b27e5b9aa5289592ba645
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-08-29 21:21:04 +0530

    Create Total sum of its patient's height is greater than or equal to 7000

---
+++ Total sum of its patient's height is greater than or equal to 7000.sql
@@ -0,0 +1,6 @@
+-- Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.
+
+select province_id, sum(height) as sum_height
+from patients
+group by province_id
+having sum_height >= 7000;
