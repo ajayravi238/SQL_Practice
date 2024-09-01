c3dce5fd6990f55cdb8e41d416916ea8f0cea1ac
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2024-09-01 21:37:19 +0530

    Create For each doctor, display their id, full name, and the first and last admission date they attended.

---
+++ For each doctor, display their id, full name, and the first and last admission date they attended..sql
@@ -0,0 +1,9 @@
+For each doctor, display their id, full name, and the first and last admission date they attended.
+
+select doctor_id,
+  first_name || ' ' || last_name as full_name,
+  min(admission_date) as first_admission_date,
+  max(admission_date) as last_admission_date
+from admissions a
+  join doctors ph on a.attending_doctor_id = ph.doctor_id
+group by doctor_id;
